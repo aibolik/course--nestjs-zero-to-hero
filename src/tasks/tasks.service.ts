eb1dacb186e8122c563b34ca6040914a12a0d5c9
Aibol Kussain <aibolikdev@gmail.com>
2020-12-02 22:37:06 +0100

    feat: add filtering, patching and deleting tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v1 as uuidv1 } from 'uuid';
 import { CreateTaskDto } from './dto/create-task-dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter-dto';
 
 @Injectable()
 export class TasksService {
@@ -9,6 +10,44 @@ export class TasksService {
 
   getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+    const { status, search } = filterDto;
+
+    let tasks = this.getAllTasks();
+
+    if (status) {
+      tasks = tasks.filter((task) => task.status === status);
+    }
+    if (search) {
+      tasks = tasks.filter(
+        (task) =>
+          task.title.includes(search) || task.description.includes(search),
+      );
+    }
+
+    return tasks;
+  }
+
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => id === task.id);
+  }
+
+  deleteTaskById(id: string) {
+    const idx = this.tasks.findIndex((task) => id === task.id);
+    this.tasks.splice(idx);
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus): Task {
+    const idx = this.tasks.findIndex((task) => task.id === id);
+    const task = {
+      ...this.tasks[idx],
+      status,
+    };
+    this.tasks.splice(idx, 1, task);
+
+    return task;
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
