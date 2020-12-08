616d40330aef7e02b8205aad85da6d37e43de52c
Aibol Kussain <aibolikdev@gmail.com>
2020-12-08 22:07:07 +0100

    feat: add configuration management with `config`

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as config from 'config';
+
+const serverConfig = config.get('server');
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
-
-  const port = 3000;
-
   const app = await NestFactory.create(AppModule);
+
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  }
+
+  const port = process.env.PORT || serverConfig.port;
+
   await app.listen(port);
 
   logger.log(`Application is listening on port ${port}`);
